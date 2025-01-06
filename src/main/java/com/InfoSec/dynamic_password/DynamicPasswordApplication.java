e99bc2dc1f69190d426eac7f54cf62e26a961290
Jokebear777 <korae9955@naver.com>
2025-01-06 21:03:17 +0900

    init

---
+++ src/main/java/com/InfoSec/dynamic_password/DynamicPasswordApplication.java
@@ -0,0 +1,13 @@
+package com.InfoSec.dynamic_password;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicPasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicPasswordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InfoSec/dynamic_password/DynamicPasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InfoSec.dynamic_password;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicPasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
